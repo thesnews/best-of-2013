a0da795179f4c76718b68aff4ef9853dc26b040c
Mike Joseph <mike.joseph5@gmail.com>
2013-01-08 21:09:33 +0000

    categories in json file for this pass

---
+++ categories.js
@@ -0,0 +1,85 @@
+module.exports = [
+    {
+        "group": "Food",
+        "categories": [
+            {"id": "f1", "name": "Best Overall Restaurant", "prepop": ["Test", "Testy"]},
+            {"id": "f2", "name": "Best Pizza"},
+            {"id": "f3", "name": "Best Burger"},
+            {"id": "f4", "name": "Best Wings"},
+            {"id": "f5", "name": "Best Sushi"},
+            {"id": "f6", "name": "Best Chinese"},
+            {"id": "f7", "name": "Best Thai"},
+            {"id": "f8", "name": "Best Mexican"},
+            {"id": "f9", "name": "Best Mediterranean Restaurant"},
+            {"id": "f10", "name": "Best Italian"},
+            {"id": "f11", "name": "Best Dessert"},
+            {"id": "f12", "name": "Best Sandwich Place"},
+            {"id": "f13", "name": "Best Breakfast Place"},
+            {"id": "f14", "name": "Best Steakhouse"},
+            {"id": "f15", "name": "Best Late Night"},
+            {"id": "f16", "name": "Best Place to Eat OnCampus"},
+            {"id": "f17", "name": "Best Coffee House"},
+            {"id": "f18", "name": "Best Place to Be Spoiled By Your Parents"},
+            {"id": "f19", "name": "Best Meal under $5"},
+            {"id": "f20", "name": "Best Bakery"},
+            {"id": "f21", "name": "Best Place to Take A Date"}
+        ]
+    },
+    {
+        "group": "Drinks",
+        "categories": [
+            {"id": "d1", "name": "Best Happy Hour/Drink Specials"},
+            {"id": "d2", "name": "Best Cocktail"},
+            {"id": "d3", "name": "Best Margarita"},
+            {"id": "d4", "name": "Best East Lansing Bar"},
+            {"id": "d5", "name": "Best Lansing Bar"},
+            {"id": "d6", "name": "Best Dance Spot"},
+            {"id": "d7", "name": "Best Karaoke"}
+        ]
+    },
+    {
+        "group": "Services",
+        "categories": [
+            {"id": "s1", "name": "Best Salon/Spa"},
+            {"id": "s2", "name": "Best Tanning Salon"},
+            {"id": "s3", "name": "Best Gym"},
+            {"id": "s4", "name": "Best Massage"},
+            {"id": "s5", "name": "Best Bike/Scooter Shop"}
+        ]
+    },
+    {
+        "group": "Stores",
+        "categories": [
+            {"id": "o1", "name": "Best Grocery Store"},
+            {"id": "o2", "name": "Best Party Store"},
+            {"id": "o3", "name": "Best Bookstore"},
+            {"id": "o4", "name": "Best Cell Phone Service"},
+            {"id": "o5", "name": "Best Flowershop"},
+            {"id": "o6", "name": "Best Sporting Goods Store"},
+            {"id": "o7", "name": "Best Clothing Store"},
+            {"id": "o8", "name": "Best Home Décor Store"},
+            {"id": "o9", "name": "Best Shoe Store"},
+            {"id": "o10", "name": "Best Craft/Hobby Store"}
+        ]
+    },
+    {
+        "group": "Living",
+        "categories": [
+            {"id": "l1", "name": "Best On Campus Housing"},
+            {"id": "l2", "name": "Best Apartment Complex"}
+        ]
+    },
+    {
+        "group": "Other",
+        "categories": [
+            {"id": "t1", "name": "Best Movie Theater"},
+            {"id": "t2", "name": "Best Place to Study"},
+            {"id": "t3", "name": "Best Bank"},
+            {"id": "t4", "name": "Best MSU Merchandise"},
+            {"id": "t5", "name": "Best New Business"},
+            {"id": "t6", "name": "Best Unique Business"},
+            {"id": "t7", "name": "Best Hidden Gem"},
+            {"id": "t8", "name": "Best Student Publication"}
+        ]
+    }
+];
